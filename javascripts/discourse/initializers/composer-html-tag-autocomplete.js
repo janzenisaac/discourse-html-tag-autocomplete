d77f6c8b0578603a46fa53f7868d3ce12a85bfa7
Mark VanLandingham <markvanlan@gmail.com>
2022-02-23 15:57:59 -0600

    Change to return object for autocomplete function

--- javascripts/discourse/initializers/composer-html-tag-autocomplete.js
+++ javascripts/discourse/initializers/composer-html-tag-autocomplete.js
@@ -5,65 +5,65 @@ import TextareaTextManipulation from "discourse/mixins/textarea-text-manipulatio
 import { getRegister } from "discourse-common/lib/get-owner";
 import { htmlTagSearch } from "../lib/html-tag-search";
 
+let lastTerm;
+const applyHtmlTagAutocomplete = function(composer) {
+  return {
+    template: findRawTemplate("html-tag-autocomplete"),
+    key: "<",
+    afterComplete: (text) => {
+      composer.set("value", text);
+      composer._focusTextArea();
+      let selection = composer.getSelected();
+      composer.selectText(selection.pre.length - lastTerm.length - 4, 0);
+    },
+    treatAsTextarea: true,
+    transformComplete: (v) => {
+      if (v.code) {
+        lastTerm = v.code;
+        return `${v.code}></${v.code}>`;
+      } else {
+        textarea.autocomplete({ cancel: true });
+        return "";
+      }
+    },
+    dataSource: (term) => {
+      return new Promise((resolve) => {
+        const full = `${term}`;
+        term = term.toLowerCase();
+
+        if (term === "") {
+          return resolve([
+                         "strike",
+                         "kbd",
+                         "b",
+                         "em",
+                         "mark",
+                         "small",
+          ]);
+        }
+
+        const options = htmlTagSearch(term);
+
+        return resolve(options);
+      }).then((list) => {
+        if (list === SKIP) {
+          return;
+        }
+        return list.map((code) => ({ code }));
+      });
+    },
+  }
+};
+
 export default {
   name: "discourse-html-tag-autocomplete",
   initialize() {
     withPluginApi("0.8.7", (api) => {
+      api.addExtraComposerAutocomplete(applyHtmlTagAutocomplete);
+
       const chat = api.container.lookup("service:chat");
-
       if (chat) {
-        chat.addAutocompleteFn(testFn);
-      }
-
-      let term;
-      function testFn(composer) {
-        const textarea = $(composer._textarea);
-        textarea.autocomplete({
-          template: findRawTemplate("html-tag-autocomplete"),
-          key: "<",
-          afterComplete: (text) => {
-            composer.set("value", text);
-            composer._focusTextArea();
-            let selection = composer.getSelected();
-            composer.selectText(selection.pre.length - term.length - 4, 0);
-          },
-          treatAsTextarea: true,
-          transformComplete: (v) => {
-            if (v.code) {
-              term = v.code;
-              return `${v.code}></${v.code}>`;
-            } else {
-              textarea.autocomplete({ cancel: true });
-              return "";
-            }
-          },
-          dataSource: (term) => {
-            return new Promise((resolve) => {
-              const full = `${term}`;
-              term = term.toLowerCase();
-
-              if (term === "") {
-                return resolve([
-                  "strike",
-                  "kbd",
-                  "b",
-                  "em",
-                  "mark",
-                  "small",
-                ]);
-              }
-
-              const options = htmlTagSearch(term);
-
-              return resolve(options);
-            }).then((list) => {
-              if (list === SKIP) {
-                return;
-              }
-              return list.map((code) => ({ code }));
-            });
-          },
-        });
+        chat.addAutocompleteFn(applyHtmlTagAutocomplete);
       }
     });
   },
